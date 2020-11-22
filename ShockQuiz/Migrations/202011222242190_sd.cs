cc0142c40b7e294353b9fc458e262bf4f8f89bf4
Santiago Gerhart <gerhartsantiago@gmail.com>
2020-11-22 20:05:24 -0300

    a

---
+++ ShockQuiz/Migrations/202011222242190_sd.cs
@@ -0,0 +1,18 @@
+﻿namespace ShockQuiz.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class sd : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Sesiones", "SesionFinalizada", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Sesiones", "SesionFinalizada");
+        }
+    }
+}
