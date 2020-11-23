4878430b3c77ebc38971148296b6e62223bac727
Santiago Gerhart <gerhartsantiago@gmail.com>
2020-11-23 19:37:20 -0300

    Nuevo Timer (agane)

--- ShockQuiz/Forms/AyudanteTimer.cs
+++ ShockQuiz/Forms/AyudanteTimer.cs
@@ -15,32 +15,61 @@ namespace ShockQuiz.Forms
         public double TiempoTranscurrido { get; set; }
         public System.Timers.Timer TimerActivo { get; set; }
 
+        public BackgroundWorker bgWorker = new BackgroundWorker();
+
         public AyudanteTimer(int pTiempoLimite)
         {
-            int INTERVALO_DE_TIEMPO = 100;
+            this.bgWorker.WorkerReportsProgress = true;
+            this.bgWorker.WorkerSupportsCancellation = true;
+            this.bgWorker.DoWork += new DoWorkEventHandler(this.bgWorker_DoWork);
+            this.bgWorker.ProgressChanged += new ProgressChangedEventHandler(this.bgWorker_ProgressChanged);
+            this.bgWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted);
+
             TiempoLimite = pTiempoLimite;
-            TimerActivo = new System.Timers.Timer(INTERVALO_DE_TIEMPO);
-            TimerActivo.Elapsed += OnTimedEvent;
-            TimerActivo.Start();
+            bgWorker.RunWorkerAsync();
         }
 
-        public void OnTimedEvent(Object source, ElapsedEventArgs e)
+        private void bgWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
-            TiempoTranscurrido += 0.1;
-            if ((TiempoLimite - TiempoTranscurrido) <= 0)
+            for (int i = TiempoLimite; i > 0; i--)
             {
-                TimerActivo.Stop();
-                foreach (Form form in Application.OpenForms)
+                if (bgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                Thread.Sleep(1000);
+                bgWorker.ReportProgress(i);
+            }
+        }
+
+        private void bgWorker_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            TiempoTranscurrido += 1;
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                List<Form> formList = new List<Form>();
+                foreach (Form item in Application.OpenForms)
+                {
+                    formList.Add(item);
+                }
+
+                foreach (Form form in formList)
                 {
                     if (form.GetType() == typeof(SesionForm))
                     {
+                        bgWorker.CancelAsync();
+
                         ((SesionForm)form).FinTiempoLimite();
+                        break;
                     }
                 }
-
             }
-
         }
-
     }
 }
